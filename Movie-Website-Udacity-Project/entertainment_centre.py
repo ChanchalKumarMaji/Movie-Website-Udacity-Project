c48c99c07994aceec30458768484f5fa9fab7c34
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2018-09-01 13:14:56 +0530

    Code Files added

---
+++ Movie-Website-Udacity-Project/entertainment_centre.py
@@ -0,0 +1,103 @@
+import fresh_tomatoes
+import media
+
+
+
+
+#toy_story=media.Movie("Toy Story","A story of a boy and his toys that came alive","https://www.google.co.in/search?q=toy+story&client=ubuntu&hs=4ZD&channel=fs&dcr=0&tbm=isch&source=iu&ictx=1&fir=9VbhPEnL0H2M3M%253A%252CW9_3ARKID5GwxM%252C_&usg=__ETKpDVUjV0tPpISLmqePASHDh4g%3D&sa=X&ved=0ahUKEwig_Z3Eq9LXAhVMK48KHUSiASkQ_h0I9wEwHA#","https://youtu.be/KYz2wyBy3kc?t=27")
+
+
+
+#print(toy_story.storyline)
+#toy_story.show_trailer()
+
+
+#avatar=media.Movie("Avatar","Alien ! Alien ! Alien!!!!! ","http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp","https://youtu.be/RqHI0kE-cyo?t=4")
+
+
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+
+
+
+
+
+
+
+
+
+Johnny_English=media.Movie("Johnny English","An evil smuggler plans to steal the Crown Jewels of the United Kingdom. After the country's top agents are killed, the only remaining hope is Johnny English, a rather unintelligent spy.","https://images-na.ssl-images-amazon.com/images/I/813Btg-Qz%2BL._SY355_.jpg","https://youtu.be/CwMmqNndnQM?t=7")
+
+
+X_Men_Apocalypse=media.Movie("X-Men: Apocalypse","The all-powerful mutant Apocalypse who is long revered as a god wants to cause extinction on earth. The X-Men must work together to demolish his plans.","http://t1.gstatic.com/images?q=tbn:ANd9GcTZX8DdBopgYG9P-vi0mwFvu9p_zmLho9ZVmVqQnJFlWAZT6-rB","https://youtu.be/Z1REP3E2Emc?t=7")
+
+
+Spiderman_Homecoming=media.Movie("Spider-Man: Homecoming","Thrilled by his experience with the Avengers, young Peter Parker returns home to live with his Aunt May. Under the watchful eye of mentor Tony Stark, Parker starts to embrace his newfound identity as Spider-Man. He also tries to return to his normal daily routine -- distracted by thoughts of proving himself to be more than just a friendly neighborhood superhero. Peter must soon put his powers to the test when the evil Vulture emerges to threaten everything that he holds dear.","https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg","https://youtu.be/H1wVask5neY?t=4")
+
+
+Independence_Day=media.Movie("Independence Day: Resurgence","When earth faces threat from species from outer space, the space defence team on the planet put up the strongest fight ever.","http://t3.gstatic.com/images?q=tbn:ANd9GcRT72LsHV2jay5G3xMjhc_xwef_J0zSjbKih1PvOvYyyytr0501","https://youtu.be/ihO6YczBl_Y?t=3")
+
+
+Expendables_3=media.Movie("The Expendables 3","Barney, Drummer, Trench and the rest of the crew reunite to fight against Stonebanks, the co-founder of The Expendables, who is now a ruthless illegal arms trader.","http://t1.gstatic.com/images?q=tbn:ANd9GcQZoOBzv-qrPekZBTS7Fl52AVNFPUNqKhY_u3zu2nVbOAa14s1m","https://youtu.be/_qu4mMjBWe8?t=3")
+
+
+The_BFG=media.Movie("The BFG","A young girl, Sophie, befriends a giant, who is an outcast because, unlike other giants, he refuses to hurt children. However, things change after the other giants decide to hunt down Sophie.","http://t0.gstatic.com/images?q=tbn:ANd9GcQozIgpFTI7eAmi16av-piJo9D34XhErw4X0zTSGj5z8gqF0egv","https://youtu.be/VG5MtenlP-A?t=12")
+
+
+Mohicans=media.Movie("The Last of the Mohicans","Amidst the British and French battle in colonial America, Hawkeye, the adopted son of a Mohican scout, romances Cora, a British officer's daughter.","http://www.gstatic.com/tv/thumb/movieposters/14197/p14197_p_v8_ah.jpg","https://youtu.be/yaQeVnN6pUc?t=10")
+
+
+Now_You_See_Me=media.Movie("Now You See Me 2","After fleeing from a stage show, the illusionists (Jesse Eisenberg, Woody Harrelson) known as the Four Horsemen find themselves in more trouble in Macau, China. Devious tech wizard Walter Mabry (Daniel Radcliffe) forces the infamous magicians to steal a powerful chip that can control all of the world's computers. Meanwhile, vengeful FBI agent Dylan Rhodes (Mark Ruffalo) hatches his own plot against Thaddeus Bradley (Morgan Freeman), the man he blames for the death of his father.","http://t3.gstatic.com/images?q=tbn:ANd9GcRjYRoNdaikER28TpUic4eKek-asQ0VN3335De6sAZOD7RJoXOl","https://youtu.be/19Q0yC7ldFk?t=4")
+
+
+Holiday_Mr_Bean=media.Movie("Mr. Bean's Holiday","Mr. Bean wins a holiday to France and sets out on his journey. Once there, he unknowingly separates a father-son duo and then decides to reunite them.","http://www.gstatic.com/tv/thumb/movieposters/163151/p163151_p_v8_an.jpg","https://youtu.be/ZV_HYwmEi_k?t=8")
+
+
+Avengers=media.Movie("Avengers: Age of Ultron","Tony Stark builds an artificial intelligence system named Ultron with the help of Bruce Banner. When the sentient Ultron makes plans to wipe out the human race, the Avengers set out to stop him.","http://t0.gstatic.com/images?q=tbn:ANd9GcRlGeugacRkKznDOtRhUCVt0AkrOTPbaaKwF9xgGZgNViyC_Xko","https://youtu.be/JRbbgeLcZ54?t=10")
+
+
+
+Wonder_Woman=media.Movie("Wonder Woman","Before she was Wonder Woman (Gal Gadot), she was Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise, Diana meets an American pilot (Chris Pine) who tells her about the massive conflict that's raging in the outside world. Convinced that she can stop the threat, Diana leaves her home for the first time. Fighting alongside men in a war to end all wars, she finally discovers her full powers and true destiny.","https://images-na.ssl-images-amazon.com/images/I/91I2JspDFLL._RI_.jpg","https://youtu.be/T5bj_fvwVTE?t=4")
+
+
+Logan=media.Movie("Logan","In the near future, a weary Logan (Hugh Jackman) cares for an ailing Professor X (Patrick Stewart) at a remote outpost on the Mexican border. His plan to hide from the outside world gets upended when he meets a young mutant (Dafne Keen) who is very much like him. Logan must now protect the girl and battle the dark forces that want to capture her.","http://filmmusicreporter.com/wp-content/uploads/2017/02/logan.jpg","https://youtu.be/m9J3SQSU7K4?t=3")
+
+
+
+
+
+
+
+
+
+
+movies=[Johnny_English,X_Men_Apocalypse,Spiderman_Homecoming,Independence_Day,Expendables_3,The_BFG,Mohicans,Now_You_See_Me,Holiday_Mr_Bean,Avengers,Wonder_Woman,Logan]
+fresh_tomatoes.open_movies_page(movies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
